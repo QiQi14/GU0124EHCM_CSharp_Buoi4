4eefd6a91eecb8423c424b92d7b3b8aec3c90c75
quyphung994 <quypn994@gmail.com>
2024-03-02 21:22:17 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using Student;
+
+namespace Execution {
+    //Viết chương trình quản lý sinh viên có các chức năng sau:
+    //Hiện danh sách sinh viên
+    //Thêm sinh viên
+    //Xóa sinh viên
+    //Sửa thông tin sinh viên
+    //Thông tin sinh viên bao gồm, mã sinh viên, tên, ngày sinh, lớp học
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInterface.mainMenu();
+            StudentManager studentManager = new StudentManager();
+
+            StudentData studentData = new StudentData();
+            studentData.code = "SV1";
+            studentData.name = "test";
+            studentData.dob = "01/01/2011";
+            studentData.currentClass = "GU";
+
+            studentManager.InsertStudent(studentData);
+
+            while (true)
+            {
+                int inputOption = Convert.ToInt32(Console.ReadLine());
+                if (inputOption == 1)
+                {
+                    UserInterface.listStudent(studentManager.getListStudent());
+                } else if (inputOption == 2)
+                {
+                    studentData = new StudentData();
+                    UserInterface.addStudent(studentData);
+                    studentManager.InsertStudent(studentData);
+                    Console.WriteLine("Them thanh cong");
+
+                    Thread.Sleep(2000);
+                    UserInterface.mainMenu();
+                }
+            }
+        }
+    }
+
+    class UserInterface
+    {
+        public static void mainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Chuong trinh quan ly sinh vien");
+            Console.WriteLine("1. Hien danh sach sinh vien");
+            Console.WriteLine("2. Them sinh vien");
+            Console.WriteLine("3. Sua thong tin sinh vien");
+            Console.WriteLine("4. Xoa sinh vien");
+            Console.WriteLine("5. Thoat chuong trinh");
+        }
+        // \n xuống dòng
+        // \t tương tự với khi ấn tab, tạo 1 khoảng trắng giữa các ký tự
+        public static void listStudent(List<StudentData> students)
+        {
+            Console.WriteLine("MSSV\t|\tTen\t|\tDoB\t\t|\tClass");
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.Write(students[i].code);
+                Console.Write("\t|\t");
+                Console.Write(students[i].name);
+                Console.Write("\t|\t");
+                Console.Write(students[i].dob);
+                Console.Write("\t|\t");
+                Console.Write(students[i].currentClass);
+                Console.WriteLine();
+            }
+        }
+
+        public static void addStudent(StudentData studentData)
+        {
+            Console.WriteLine("Vui long nhap ma sinh vien");
+            studentData.code = Console.ReadLine();
+            Console.WriteLine("Vui long nhap ten sinh vien");
+            studentData.name = Console.ReadLine();
+            Console.WriteLine("Vui long nhap ngay sinh sinh vien");
+            studentData.dob = Console.ReadLine();
+            Console.WriteLine("Vui long nhap lop dang hoc");
+            studentData.currentClass = Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student {
+    public class StudentData {
+        public string code { get; set; }
+        public string name { get; set; }
+        public string dob { get; set; }
+        public string currentClass { get; set; }
+    }
+
+    //class DongVat
+    //function dongVat
+
+    public class StudentManager
+    {
+        List<StudentData> students = new List<StudentData>();
+
+        public List<StudentData> getListStudent()
+        {
+            return students;
+        }
+
+        public void InsertStudent(StudentData student)
+        {
+            students.Add(student);
+        }
+
+        public void RemoveStudentByIndex(int index)
+        {
+            students.RemoveAt(index);
+        }
+
+        public void EditStudent(StudentData newData, int studentIndex)
+        {
+            students[studentIndex] = newData;
+        }
+    }
+}
